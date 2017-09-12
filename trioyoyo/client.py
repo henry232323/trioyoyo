7927667ec425a3a2f9863dd475b704d0e604d89d
Henry <henry@rhodochrosite.xyz>
2017-09-12 16:11:38 -0600

    Update client.py

--- trioyoyo/client.py
+++ trioyoyo/client.py
@@ -148,7 +148,7 @@ class IRCClient(object):
 
 class CommandClient(IRCClient):
     """IRCClient, using a command handler"""
-    def __init__(self, cmd_handler: typing.Callable[[trioyoyo.IRCClient], trioyoyo.CommandHandler], *args, **kwargs):
+    def __init__(self, cmd_handler: typing.Callable[[IRCClient], CommandHandler], *args, **kwargs):
         """Takes a command handler (see oyoyo.cmdhandler.CommandHandler)
         whose attributes are the commands you want callable, for example
         with a privmsg cmdhandler.privmsg will be awaited with the
